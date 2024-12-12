21ad8ec4aa9326b91a128bf10d919ca8b90d69ec
soeun2537 <soeun2537@gmail.com>
2024-12-12 17:28:29 +0900

    feat: 입출력 시 언더 바 공백 대체 파싱 기능 구현

--- src/main/java/menu/util/Parser.java
+++ src/main/java/menu/util/Parser.java
@@ -1,5 +1,7 @@
 package menu.util;
 
+import static menu.constant.InputConstant.*;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,5 +12,13 @@ public class Parser {
 
     public static List<String> separateBySeparator(String input, String separator) {
         return Arrays.asList(input.split(separator));
+    }
+
+    public static String replaceUnderBarToBlank(String input) {
+        return input.replaceAll(UNDER_BAR.getContent(), BLANK.getContent());
+    }
+
+    public static String replaceBlankToUnderBar(String input) {
+        return input.replaceAll(BLANK.getContent(), UNDER_BAR.getContent());
     }
 }
