fada403a4187d668431e0c8d05ca11a8924b43a4
soeun2537 <soeun2537@gmail.com>
2024-12-12 17:34:07 +0900

    feat: 메뉴 카테고리 Category Enum 및 관련 메서드 구현

---
+++ src/main/java/menu/model/Category.java
@@ -0,0 +1,19 @@
+package menu.model;
+
+public enum Category {
+    일식,
+    한식,
+    중식,
+    아시안,
+    양식
+    ;
+
+    public static Category recommendCategory(int randomNumber) {
+        for (Category category : values()) {
+            if (category.ordinal() + 1 == randomNumber) {
+                return category;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
